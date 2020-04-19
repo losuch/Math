adda09b7b86ca93d25050481607c8ccb416a5ece
Lukasz Osuch <losuch@gmail.com>
2020-04-19 11:30:21 +0200

    init

---
+++ Math/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Math
+//
+//  Created by Lukasz Osuch on 19.04.20.
+//  Copyright © 2020 Lukasz Osuch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
